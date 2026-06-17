425ca8935e20300e709effef3fe1266f0cab4595
mohit <mohitsaraswat39999@gmail.com>
2026-06-17 19:53:00 +0530

    create heathcheck controller and route

---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,19 @@
+import { ApiResponse } from "../utils/api-respose.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+/**
+ 
+const healthCheck = (req, res) => {
+  try {
+    res
+      .status(200)
+      .json(new ApiResponse(200, { message: "server is running" }));
+  } catch (error) {}
+};
+ */
+
+const healthCheck = asyncHandler((req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "server is running" }));
+});
+
+export { healthCheck };
